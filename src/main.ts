7328387829905abf043f894c16fd17a4c89cef19
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2022-09-29 10:01:18 +0100

    ADD: Introduce graphQL

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,31 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp, h, provide } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import router from "./router";
+import App from './App.vue';
+import router from './router';
 
-import "./assets/main.css";
+import './assets/main.css';
 
-const app = createApp(App);
+import { DefaultApolloClient } from '@vue/apollo-composable';
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  cache,
+  //   uri: "https://rickandmortyapi.com/graphql",
+  uri: import.meta.env.VITE_API_URL,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+});
 
 app.use(createPinia());
 app.use(router);
 
-app.mount("#app");
+app.mount('#app');
