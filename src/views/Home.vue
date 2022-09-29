50205a5a88b992165c892dec113826cedf5cc58d
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2022-09-29 07:23:16 +0100

    ADD: Create Vite App

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <change-user-modal v-if="isStarting" />
+  </div>
+</template>
+
+<script>
+import ChangeUserModal from '@/components/ChangeUserModal.vue';
+import { mapState } from 'vuex';
+
+export default {
+  name: 'Home',
+  components: {
+    ChangeUserModal,
+  },
+
+  computed: {
+    ...mapState(['isStarting']),
+  },
+
+  created() {
+    this.$store.commit('SET_STARTING', true);
+  },
+};
+</script>
