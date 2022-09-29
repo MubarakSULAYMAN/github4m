545f730a853198cf3f596a57a7b8e8d2bfb9a5ac
MubarakSULAYMAN <14-30gr092@unilorin.edu.ng>
2022-09-29 09:54:56 +0100

    UPDATE: Add environment variables types

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string;
+  readonly VITE_GRAPHQL_URL: string;
+  readonly VITE_BASE_URL: string;
+  readonly VITE_API_URL: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
